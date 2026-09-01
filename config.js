788c2fd979034680bd88c2b051bb60b8645aa487
mopvps <automationmopower@gmail.com>
2026-09-01 11:58:12 +0530

    config logis addes

---
+++ config.js
@@ -0,0 +1,9 @@
+const APP_CONFIG = {
+  tabs: {
+    1: { enabled: true, label: 'Footnote Linker Internal' },
+    2: { enabled: true, label: 'Footnote Link External' },
+    3: { enabled: true, label: 'Reference Linker Ext' },
+    4: { enabled: true, label: 'Content Linker' },
+    5: { enabled: false, label: 'Sup Tagger' },
+  }
+};
